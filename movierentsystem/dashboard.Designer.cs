ad7d83f14c9647bd224798c13472f4ca71a6d47a
Martin <martindolores65@gmail.com>
2022-06-03 20:27:04 +1000

    New Dashboard UI

--- movierentsystem/dashboard.Designer.cs
+++ movierentsystem/dashboard.Designer.cs
@@ -29,12 +29,143 @@ namespace loginForm
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dashboard));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.moviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewMovieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addCustomerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewCustomerInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.issueMovieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnMoviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.completeMovieDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moviesToolStripMenuItem,
+            this.customerToolStripMenuItem,
+            this.issueMovieToolStripMenuItem,
+            this.returnMoviesToolStripMenuItem,
+            this.completeMovieDetailsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1160, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // moviesToolStripMenuItem
+            // 
+            this.moviesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewMovieToolStripMenuItem,
+            this.viewMoviesToolStripMenuItem});
+            this.moviesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("moviesToolStripMenuItem.Image")));
+            this.moviesToolStripMenuItem.Name = "moviesToolStripMenuItem";
+            this.moviesToolStripMenuItem.Size = new System.Drawing.Size(90, 24);
+            this.moviesToolStripMenuItem.Text = "Movies";
+            // 
+            // addNewMovieToolStripMenuItem
+            // 
+            this.addNewMovieToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addNewMovieToolStripMenuItem.Image")));
+            this.addNewMovieToolStripMenuItem.Name = "addNewMovieToolStripMenuItem";
+            this.addNewMovieToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addNewMovieToolStripMenuItem.Text = "Add New Movie";
+            // 
+            // viewMoviesToolStripMenuItem
+            // 
+            this.viewMoviesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewMoviesToolStripMenuItem.Image")));
+            this.viewMoviesToolStripMenuItem.Name = "viewMoviesToolStripMenuItem";
+            this.viewMoviesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.viewMoviesToolStripMenuItem.Text = "View Movies";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addCustomerToolStripMenuItem,
+            this.viewCustomerInfoToolStripMenuItem});
+            this.customerToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("customerToolStripMenuItem.Image")));
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(106, 24);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // addCustomerToolStripMenuItem
+            // 
+            this.addCustomerToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addCustomerToolStripMenuItem.Image")));
+            this.addCustomerToolStripMenuItem.Name = "addCustomerToolStripMenuItem";
+            this.addCustomerToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addCustomerToolStripMenuItem.Text = "Add Customer";
+            // 
+            // viewCustomerInfoToolStripMenuItem
+            // 
+            this.viewCustomerInfoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewCustomerInfoToolStripMenuItem.Image")));
+            this.viewCustomerInfoToolStripMenuItem.Name = "viewCustomerInfoToolStripMenuItem";
+            this.viewCustomerInfoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.viewCustomerInfoToolStripMenuItem.Text = "View Customer Info";
+            // 
+            // issueMovieToolStripMenuItem
+            // 
+            this.issueMovieToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("issueMovieToolStripMenuItem.Image")));
+            this.issueMovieToolStripMenuItem.Name = "issueMovieToolStripMenuItem";
+            this.issueMovieToolStripMenuItem.Size = new System.Drawing.Size(126, 24);
+            this.issueMovieToolStripMenuItem.Text = "Issue Movies";
+            // 
+            // returnMoviesToolStripMenuItem
+            // 
+            this.returnMoviesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("returnMoviesToolStripMenuItem.Image")));
+            this.returnMoviesToolStripMenuItem.Name = "returnMoviesToolStripMenuItem";
+            this.returnMoviesToolStripMenuItem.Size = new System.Drawing.Size(137, 24);
+            this.returnMoviesToolStripMenuItem.Text = "Return Movies";
+            // 
+            // completeMovieDetailsToolStripMenuItem
+            // 
+            this.completeMovieDetailsToolStripMenuItem.Name = "completeMovieDetailsToolStripMenuItem";
+            this.completeMovieDetailsToolStripMenuItem.Size = new System.Drawing.Size(183, 24);
+            this.completeMovieDetailsToolStripMenuItem.Text = "Complete Movie Details";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1160, 643);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem moviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewMovieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewMoviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addCustomerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewCustomerInfoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem issueMovieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnMoviesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem completeMovieDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
